5d77ab557235e025b68b1fa2c168289719cfaa8b
Pedro Contreras Avendano <45802239+pcavendano@users.noreply.github.com>
2024-02-18 20:04:27 -0500

    Remove tsx from the project

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
@@ -9,16 +8,14 @@ const roboto = Roboto({
   variable: "--ff-roboto",
 });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Portfolio AI",
   description: "Créé des portfolios pour les étudiants à la recherche d'un stage",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={cn("font-roboto antialiased, roboto.variable")}>{children}</body>
