5ae8de329c4357e74c63513a7aa02d81beccd2f4
Pedro Contreras Avendano <45802239+pcavendano@users.noreply.github.com>
2024-02-19 11:56:27 -0500

    modify sidebar

--- components/shared/sidebar.js
+++ components/shared/sidebar.js
@@ -6,6 +6,7 @@ import { usePathname } from "next/navigation";
 import Image from "next/image";
 import { navLinks } from "@/constants.js";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 const Sidebar = () => {
     const pathname = usePathname();
@@ -16,7 +17,7 @@ const Sidebar = () => {
                 <nav className="sidebar-nav">
                     <SignedIn>
                         <ul className="sidebar-nav_elements">
-                            {navLinks.map((link) => {
+                            {navLinks.slice(0, 6).map((link) => {
                                 const isActive = link.route === pathname;
                                 return (
                                     <li
@@ -45,6 +46,40 @@ const Sidebar = () => {
                                     </li>
                                 );
                             })}
+                        </ul>
+                        <ul>
+                            {navLinks.slice(6).map((link) => {
+                                const isActive = link.route === pathname;
+                                return (
+                                    <li
+                                        key={link.route}
+                                        className={`sidebar-nav_element group ${
+                                            isActive
+                                                ? "bg-purple-gradient text-white"
+                                                : "text-gray-700"
+                                        }`}
+                                    >
+                                        <Link
+                                            className="sidebar-link"
+                                            href={link.route}
+                                        >
+                                            <Image
+                                                src={link.icon}
+                                                alt="logo"
+                                                width={24}
+                                                height={24}
+                                                className={`${
+                                                    isActive && "brightness-200"
+                                                }`}
+                                            />
+                                            {link.label}
+                                        </Link>
+                                    </li>
+                                );
+                            })}
+                            <li className="">
+                                <UserButton afterSignOutUrl="/" showName />
+                            </li>
                         </ul>
                     </SignedIn>
                     <SignedOut>
