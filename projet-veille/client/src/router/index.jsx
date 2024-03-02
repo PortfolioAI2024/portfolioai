c147c978c6b329139da223ef0c281616b41bd893
Pedro Contreras Avendano <45802239+pcavendano@users.noreply.github.com>
2024-03-02 12:31:44 -0500

    fix redirect to home in sidemenu

--- projet-veille/client/src/router/index.jsx
+++ projet-veille/client/src/router/index.jsx
@@ -6,7 +6,7 @@ import NoMatch from "../NoMatch.jsx";
 import Root from "../Root.jsx";
 import Login from "../components/Login/Login.jsx";
 import SignUp from "../SignUp.jsx";
-import Profile from "../Profile.jsx";
+import Profile from "../components/Profile/Profile.jsx";
 import CreateChatbot from "../CreateChatbot.jsx";
 import Home from "../components/home/Home.jsx";
 
